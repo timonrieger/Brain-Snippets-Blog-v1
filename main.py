1ae650577c1868ae96280a814ca2a9dd3d547727
timonrieger <github.com.kindle922@passmail.net>
2025-01-07 20:07:34 +0100

    refactor: rename endpoints function names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, abort, render_template, redirect, send_from_directory, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from forms import CreatePostForm
@@ -24,7 +24,7 @@ except Exception:
         blog_data = json.load(file)
 
 @app.route('/')
-def get_all_posts():
+def home():
     page = int(request.args.get("page", 1))
     posts_per_page = 10
     start = (page - 1) * posts_per_page
@@ -55,7 +55,7 @@ def show_post(post_title):
     return render_template("post.html", post=post)
 
 @app.route("/new", methods=["GET", "POST"])
-def add_new_post():
+def new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
 
@@ -71,7 +71,7 @@ def add_new_post():
 
         dump_and_copy(new_post_data)
 
-        return redirect(url_for("get_all_posts"))
+        return redirect(url_for("home"))
     return render_template("make-post.html", form=form)
 
 @app.route("/edit/<int:post_id>", methods=["GET", "POST"])
@@ -93,7 +93,7 @@ def edit_post(post_id):
 
         dump_and_copy(post)
 
-        return redirect(url_for("get_all_posts"))
+        return redirect(url_for("home"))
     return render_template("make-post.html", form=form)
 
 def dump_and_copy(data: dict):
